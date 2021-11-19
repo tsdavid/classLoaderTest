98a68d07c0c1a0c0526021a857cbe0f4aa98c7b9
tsdavid <ipackcorpdavid@gmail.com>
2021-11-19 23:35:15 +0900

    Sample of ClassLoader Test

--- TestClassLoader/src/com/tibco/dk/test/main/TestClassLoaderWithJava.java
+++ TestClassLoader/src/com/tibco/dk/test/main/TestClassLoaderWithJava.java
@@ -1,5 +1,42 @@
 package com.tibco.dk.test.main;
 
+import java.io.File;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class TestClassLoaderWithJava {
 
+	public static void main(String[] args) {
+		
+		
+		try{
+			String filePath = "..\\classLoaderTest\\DTO\\";
+			String packageName = "com.tibco.dk.dto.SampleDTO";
+			
+			File file = new File(filePath);
+			
+			for(String f : file.list()){
+				System.out.println(f);
+			}
+			
+			// Convert the file to URL Format
+			URL url = file.toURI().toURL();
+			URL[] urls = new URL[]{url};
+			
+			// Load this folder into Class Loader.
+			ClassLoader cl = new URLClassLoader(urls);
+			
+			// Load the Address class in file.
+			Class<?> clazz = cl.loadClass(packageName);
+			
+			for(Field field : clazz.getDeclaredFields()){
+				System.out.println(field.getName());
+			}
+			
+		}catch(Exception e){
+			e.getMessage();
+			e.printStackTrace();
+		}
+	}
 }
--- classLoaderTest/src/com/tibco/dk/test/GlobalInstanceTestLoader.java
+++ classLoaderTest/src/com/tibco/dk/test/GlobalInstanceTestLoader.java
@@ -1,5 +1,10 @@
 package com.tibco.dk.test;
 
+import java.io.File;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class GlobalInstanceTestLoader {
 	
 	public GlobalInstanceTestLoader() {
@@ -26,5 +31,39 @@ public class GlobalInstanceTestLoader {
 	
 	public void instatiateDTO_Class(String filePath, String packageName){
 		
+		ClassLoader bwClassLoader = Thread.currentThread().getContextClassLoader();
+		
+		try {
+			Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+			
+			File file = new File(filePath);
+			
+			for(String f : file.list()){
+				System.out.println(f);
+			}
+			
+			// Convert the file to URL Format
+			URL url = file.toURI().toURL();
+			URL[] urls = new URL[]{url};
+			
+			// Load this folder into Class Loader.
+			ClassLoader cl = new URLClassLoader(urls);
+			
+			// Load the Address class in file.
+			Class<?> clazz = cl.loadClass(packageName);
+			
+			for(Field field : clazz.getDeclaredFields()){
+				System.out.println(field.getName());
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.getMessage();
+			e.printStackTrace();
+			
+		}finally{
+			Thread.currentThread().setContextClassLoader(bwClassLoader);
+			
+		}
 	}
 }
